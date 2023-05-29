c7d6af7ae491284cc4b01d807b37a6bf988cb012
density <44949218+gregoryclayton@users.noreply.github.com>
2023-05-28 21:47:36 -0400

    Add files via upload

---
+++ imageCircler/main.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from PIL import Image, ImageDraw
+
+# Open the input image as numpy array, convert to RGB
+img = Image.open("C:/Users/grego/Desktop/pros/imageCircler/pics/picture2.png").convert("RGB")
+npImage = np.array(img)
+h,w = img.size
+
+# Create same size alpha layer with circle
+alpha = Image.new('L', img.size,0)
+draw = ImageDraw.Draw(alpha)
+draw.pieslice([0,0,h,w],0,360,fill=255)
+
+# Convert alpha Image to numpy array
+npAlpha=np.array(alpha)
+
+# Add alpha layer to RGB
+npImage=np.dstack((npImage,npAlpha))
+
+# Save with alpha
+Image.fromarray(npImage).save('circle5.png')
